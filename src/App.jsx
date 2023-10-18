ab2574e81658ee87b3e62da144a4e72b8fc1baae
Somgupta786 <somvishwari786@gmail.com>
2023-10-18 22:00:19 +0530

    make a reusable card using map

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import contacts from './contacct'
 
 function Card(props){
     return (
@@ -21,28 +21,16 @@ function Card(props){
         </div>
       );
 }
-
+function placing(cont){
+<Card name={cont.name}
+    src={cont.imgURL}
+    tel={cont.phone}
+    mail={cont.email}
+/>
+}
 function App() {
-    return(<><Card 
-        name="Beyonce"
-        src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-        tel="+123 456 789"
-        mail="b@beyonce.com"
-        /> 
-        
-        <Card 
-        name="Jack Bauer"
-        src="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
-        tel="+987 654 321"
-        mail="jack@nowhere.com"
-        />
-        <Card 
-        name="Chuck Norris"
-        src="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
-        tel="+918 372 574"
-        mail="b@beyonce.com"
-        />
-        </>
+    return(<>{contacts.map(placing)
+     } </>
     
     
     
