f073d94776271cbf5e1d73af766647d228d58844
Somgupta786 <somvishwari786@gmail.com>
2023-10-18 23:06:21 +0530

    change notes

--- src/Note.jsx
+++ src/Note.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function Note(){
-  return( <div>
+  return( <div className='note'>
     <h1>This is note title</h1> 
     <p>This is a note content</p>
  </div>) ;
