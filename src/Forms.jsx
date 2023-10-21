14ee00a281f65c33448bbc438469bfae19f3ade4
Somgupta786 <somvishwari786@gmail.com>
2023-10-21 19:23:29 +0530

    make note_App editable

--- src/Forms.jsx
+++ src/Forms.jsx
@@ -1,59 +1,87 @@
-import { useState  } from "react";
+import { useState } from "react";
 import './index.css'
 import CreateNotes from "./CreateNotes";
 import Note from "./Note";
 
 let counter = 0;
 
-
 function Forms() {
-
   const [notes, setNotes] = useState([]);
   const [inputText, setInputText] = useState("");
+  const [editNotes, setEdit] = useState(null);
+  const [isStart, setStart] = useState(true);
 
   const textHandler = (e) => {
     setInputText(e.target.value);
   };
 
-  
   const saveHandler = () => {
-    setNotes((prevState) => [
-      ...prevState,
-      {
-        id: `id_${counter++}` ,
-        text: inputText
-      }
-    ]);
-  
-    setInputText("");
-  };
+    if (inputText) {
+      if (editNotes !== null) {
+       
+        setNotes((prevNotes) =>
+          prevNotes.map((note) =>
+            note.id === editNotes ? { ...note, text: inputText } : note
+          )
+        );
+        setEdit(null);
+        setStart(true); 
+        setInputText('');
+      } else {
+       
+        setNotes((prevNotes) => [
+          ...prevNotes,
+          {
+            id: `id_${counter++}`,
+            text: inputText,
+          },
+        ]);
 
+        setInputText('');
+      }
+    }
+  };
 
   const deleteNote = (id) => {
     const filteredNotes = notes.filter((note) => note.id !== id);
     setNotes(filteredNotes);
   };
 
- 
-  
+  const editNote = (id, text) => {
+    setStart(false); 
+    setEdit(id);
+    setInputText(text);
+  };
+
   return (
     <div className="notes">
-      {notes.map((note) => (
-        <Note
-          key={note.id}
-          id={note.id}
-          text={note.text}
-          deleteNote={deleteNote}
+      {notes.map((note) =>
+        editNotes === note.id ? (
+          <CreateNotes
+            key={note.id}
+            textHandler={textHandler}
+            saveHandler={saveHandler}
+            inputText={inputText}
+          />
+        ) : (
+          <Note
+            key={note.id}
+            id={note.id}
+            text={note.text}
+            editNote={editNote}
+            deleteNote={deleteNote}
+          />
+        )
+      )}
+      {isStart ? (
+        <CreateNotes
+          textHandler={textHandler}
+          saveHandler={saveHandler}
+          inputText={inputText}
         />
-      ))}
-      <CreateNotes
-        textHandler={textHandler}
-        saveHandler={saveHandler}
-        inputText={inputText}
-      />
+      ) : null}
     </div>
   );
 }
 
 export default Forms;
-
--- src/Note.jsx
+++ src/Note.jsx
@@ -1,13 +1,14 @@
 
 import React from 'react'
 
-export default function Note({ id, text, deleteNote }){
+export default function Note({ id, text, editNote,deleteNote}){
     return (
         <div className="note">
           <div className="note__body">{text}</div>
           <div className="note__footer" style={{ justifyContent: "flex-end" }}>
             
             <button className="note__delete" onClick={() => deleteNote(id) } >Delete</button>
+            <button className="note__delete"  onClick={() => editNote(id,text)} >Edit</button>
           </div>
           
         </div>
